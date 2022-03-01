e39350f9eef5301f92ca5822fb4c4f40453af468
linetlucy genchabe <linetlucy21@gmail.com>
2022-03-01 11:34:55 +0300

    add convert function

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,37 @@
-var add = function(number1, number2) {
-    return number1 + number2;
+var multiply = function(number1, number2) {
+    return number1 * number2;
   };
   
   var number1 = parseInt(prompt("Enter a number:"));
   var number2 = parseInt(prompt("Enter another number:"));
-  var result = add(number1, number2);
+  var result = multiply(number1, number2);
   alert(result);
-  var add = function(number1, number2) {
-    return number1 + number2;
+  var multiply = function(number1, number2) {
+    return number1 * number2;
   };
   
-  var number1 = parseInt(prompt("Enter a number:"));
-  var number2 = parseInt(prompt("Enter another number:"));
-  var result = devide(number1, number2);
-  alert(result);
+var divide = function(height, weight) { 
+      return height/ weight; 
+
+};
+   var height=parseInt(prompt("Enter your height:"));
+   var weight=parseInt(prompt("Enter your weight"));
+   var result= divide(height,weight);
+   alert(result);
+   var divide = function(height,weight){
+           return height/weight;
+    
+   };
+   var convert =function(celcius,num1,num2){
+       return celcius*num1+num2;
+   };
+   var celcius=parseInt(prompt("Enter celcius degree"));
+   const num1=1.8;
+   const num2=32;
+   var result=convert(celius,num1,num2);
+   alert(result);
+   var convert=function(celcius,num1,num2){
+       return celcius*num1+num2;
+   };
+
+  
