66ae4d093a1362fdc38a5ec025bc9e14e21b1a55
linetlucy genchabe <linetlucy21@gmail.com>
2022-03-01 10:58:02 +0300

    add another function

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,11 @@ var add = function(number1, number2) {
   var number2 = parseInt(prompt("Enter another number:"));
   var result = add(number1, number2);
   alert(result);
+  var add = function(number1, number2) {
+    return number1 + number2;
+  };
+  
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  var result = devide(number1, number2);
+  alert(result);
